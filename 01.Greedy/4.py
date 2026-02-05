7ff671db1c1c57fb14703f4856c093c37d8c759e
gwkh0103 <kyish2001@gmail.com>
2026-02-05 17:38:07 +0900

    주석포함

--- 01.Greedy/4.py
+++ 01.Greedy/4.py
@@ -1,16 +1,20 @@
+# N, K를 공백으로 구분하여 입력받기
 N, K = map(int, input().split())
 result = 0
-while True:
 
+while True:
+    #(N==K로 나누어 떨어지는 수)가 될 때까지 1씩 빼기
     target = (N // K) * K
     result += (N - target)
     N = target
-
-    if N < K
+    # N이 K보다 작을 때(더 이상 나눌 수 없을 때) 반복문 탈출
+    if N < K:
         break 
+    # K로 나누기
     result += 1
     N //= K
 
-result
+# 마지막으로 남은 수에 대하여 1씩 빼기
+result += (N - 1)
 print(result)
 
