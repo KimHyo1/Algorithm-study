42cceee0b77b30270fd6d678fd4b9251b69af646
gwkh0103 <kyish2001@gmail.com>
2026-02-26 15:57:13 +0900

    0226

---
+++ 04.Sorting/04.Sorting/25.py
@@ -0,0 +1,9 @@
+array = [7, 5, 9, 0, 3, 1, 6, 2, 9, 1, 4, 8, 0, 5, 2]
+count = [0] * (max(array) + 1)
+
+for i in range(len(array)):
+    count[array[i]] += 1
+
+for i in range(len(count)):
+    for j in range(count[i]):
+        print(i, end=' ') #띄어쓰기를 구분으로 등장한 횟수만큼 인덱스 출력
